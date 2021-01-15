b6709cbfeec72435978c15ed2207e9f35266b841
Gyanendra Singh <gyanendra.singh@congnizant.com>
2021-01-15 11:56:02 +0530

    Added Junit

--- src/com/cts/cts2020/test/PersonTest.java
+++ src/com/cts/cts2020/test/PersonTest.java
@@ -2,25 +2,36 @@ package com.cts.cts2020.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.cts.cts2020.Aadhaar;
+import com.cts.cts2020.NoAadharException;
 import com.cts.cts2020.Person;
 
 public class PersonTest {
+	Person p =null;
+	
+	@BeforeEach
+	void runsBeforeTest() {
+		p = new Person("Mohit","Keswani");
+	}
 	
     @Test
 	void testPersonName(){
-	Person p = new Person("Mohit","Keswani");
+	//Person p = new Person("Mohit","Keswani");
 	assertEquals("Mohit"+" "+"Keswani",p.getName());
+	
 	}
     
     @Test
-    void testPersonAAdhaar() {
-    	Person p = new Person("Mahesh","Arora");
+    void testPersonAAdhaar() throws NoAadharException {
+    	//Person p = new Person("Mahesh","Arora");
     	Aadhaar a = new Aadhaar();
     	a.setAadhaarNum(23l);
     	assertEquals(23,a.getAadhaarNum().longValue());
+    	p.setAadhaar(a);
+    	assertEquals(23, p.getAadhaar().getAadhaarNum().longValue());
     	
     }
 
